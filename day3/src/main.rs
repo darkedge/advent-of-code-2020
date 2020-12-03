5e4d4373959ad99d875e8753a6a596256728a89e
Marco Jonkers <contact@marcojonkers.com>
2020-12-03 23:24:16 +0100

    Day 3 part two first attempt

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -67,12 +67,10 @@ Starting at the top-left corner of your map and following a slope of right 3 and
 trees would you encounter?
 */
 
-mod parser;
-
 use std::fs::File;
 use std::io::prelude::*;
 
-fn count_trees(buffer: &Vec<u8>, stride: usize, right: usize, down: usize) -> i32{
+fn count_trees(buffer: &Vec<u8>, stride: usize, right: usize, down: usize) -> i32 {
     let len = buffer.len();
     let mut x = 0;
     let mut y = 0;
@@ -105,20 +103,54 @@ fn part_one() {
             let right = 3;
             let down = 1;
 
-            println!("Number of trees: {}", count_trees(&buffer, stride, right, down));
+            println!(
+                "Number of trees: {}",
+                count_trees(&buffer, stride, right, down)
+            );
         }
     }
 }
 
+/*
+--- Part Two ---
+
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal
+stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at
+the top-left corner and traverse the map all the way to the bottom:
+
+    Right 1, down 1.
+    Right 3, down 1. (This is the slope you already checked.)
+    Right 5, down 1.
+    Right 7, down 1.
+    Right 1, down 2.
+
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied
+together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed
+slopes?
+*/
+
 fn part_two() {
-    match parser::read_lines("input") {
-        Ok(lines) => {
-            for line in lines {
-                if let Ok(_entry) = line {}
+    if let Ok(mut file) = File::open("input") {
+        let mut buffer = Vec::new();
+        if let Ok(_) = file.read_to_end(&mut buffer) {
+            // Get the width of the map
+            let stride = buffer.iter().position(|&c| c == '\n' as u8).unwrap();
+
+            let mut product = 1;
+            for (right, down) in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)].iter() {
+                let count = count_trees(&buffer, stride, *right, *down);
+                println!(
+                    "Number of trees - Right {}, down {}: {}",
+                    right, down, count
+                );
+                product *= count;
             }
-        }
-        _ => {
-            println!("Error: Could not read lines!")
+
+            println!("Multiplied counts: {}", product);
         }
     }
 }
