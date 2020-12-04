73f4c9e7dfc79970fc6d55765c1db99ee15c71e0
Marco Jonkers <contact@marcojonkers.com>
2020-12-04 21:56:16 +0100

    Solution Day 4 part two

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -57,6 +57,7 @@ According to the above rules, your improved system would report 2 valid passport
 Count the number of valid passports - those that have all required fields. Treat cid as optional.
 In your batch file, how many passports are valid?
 */
+use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -69,6 +70,21 @@ struct Passport<'a> {
     eye_color: Option<&'a str>,       // ecl
     passport_id: Option<&'a str>,     // pid
     country_id: Option<&'a str>,      // cid
+}
+
+impl fmt::Debug for Passport<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Point")
+            .field("birth_year", &self.birth_year)
+            .field("issue_year", &self.issue_year)
+            .field("expiration_year", &self.expiration_year)
+            .field("height", &self.height)
+            .field("hair_color", &self.hair_color)
+            .field("eye_color", &self.eye_color)
+            .field("passport_id", &self.passport_id)
+            .field("country_id", &self.country_id)
+            .finish()
+    }
 }
 
 fn parse_passports<'a>(list: &'a Vec<&'a str>) -> Vec<Passport> {
@@ -130,7 +146,26 @@ fn parse_passports<'a>(list: &'a Vec<&'a str>) -> Vec<Passport> {
     return passports;
 }
 
-fn part_one() {
+fn part_one(passports: &Vec<Passport>) -> i32 {
+    let mut count = 0;
+
+    for x in passports {
+        if x.birth_year.is_some()
+            && x.issue_year.is_some()
+            && x.expiration_year.is_some()
+            && x.height.is_some()
+            && x.hair_color.is_some()
+            && x.eye_color.is_some()
+            && x.passport_id.is_some()
+        {
+            count += 1;
+        }
+    }
+
+    count
+}
+
+fn validate(validate: &dyn Fn(&Vec<Passport>) -> i32) {
     if let Ok(mut file) = File::open("input") {
         // Read the whole file into a string.
         let mut contents = String::new();
@@ -142,27 +177,191 @@ fn part_one() {
         println!("Number of passports: {}", passports.len());
 
         // Now we can split each password into fields.
-        let mut parsed = parse_passports(&passports);
+        let parsed = parse_passports(&passports);
 
         // Valid passports must contain certain values
-        parsed.retain(|x| {
-            x.birth_year.is_some()
-                && x.issue_year.is_some()
-                && x.expiration_year.is_some()
-                && x.height.is_some()
-                && x.hair_color.is_some()
-                && x.eye_color.is_some()
-                && x.passport_id.is_some()
-        });
-
-        println!("Number of valid passports: {}", parsed.len());
+        println!("Number of valid passports: {}", validate(&parsed));
     }
 }
 
-fn part_two() {}
+/*
+--- Part Two ---
+
+The line is moving more quickly now, but you overhear airport security talking about how passports
+with invalid data are getting through. Better add some data validation, quick!
+
+You can continue to ignore the cid field, but each other field has strict rules about what values
+are valid for automatic validation:
+
+    byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    hgt (Height) - a number followed by either cm or in:
+        If cm, the number must be at least 150 and at most 193.
+        If in, the number must be at least 59 and at most 76.
+    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    pid (Passport ID) - a nine-digit number, including leading zeroes.
+    cid (Country ID) - ignored, missing or not.
+
+Your job is to count the passports where all required fields are both present and valid according to
+the above rules. Here are some example values:
+
+byr valid:   2002
+byr invalid: 2003
+
+hgt valid:   60in
+hgt valid:   190cm
+hgt invalid: 190in
+hgt invalid: 190
+
+hcl valid:   #123abc
+hcl invalid: #123abz
+hcl invalid: 123abc
+
+ecl valid:   brn
+ecl invalid: wat
+
+pid valid:   000000001
+pid invalid: 0123456789
+
+Here are some invalid passports:
+
+eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+
+Here are some valid passports:
+
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+
+Count the number of valid passports - those that have all required fields and valid values. Continue
+to treat cid as optional. In your batch file, how many passports are valid?
+*/
+use regex::Regex;
+
+fn regex_is_match<'t>(
+    option_string: Option<&'t str>,
+    re: &regex::Regex,
+) -> Option<regex::Match<'t>> {
+    if let Some(string) = option_string {
+        //println!("String exists: {}", string);
+        if let Some(captures) = re.captures(string) {
+            return captures.get(1);
+        }
+    }
+
+    None
+}
+
+fn regex_match(option_string: Option<&str>, re: &regex::Regex, min: i32, max: i32) -> bool {
+    if let Some(match1) = regex_is_match(option_string, re) {
+        //println!("Regex passed");
+        if let Ok(val) = match1.as_str().parse::<i32>() {
+            //println!("Checking if {} is between {} and {}", val, min, max);
+            return val >= min && val <= max;
+        }
+    }
+
+    false
+}
+
+fn part_two(parsed: &Vec<Passport>) -> i32 {
+    /*
+    byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    hgt (Height) - a number followed by either cm or in:
+        If cm, the number must be at least 150 and at most 193.
+        If in, the number must be at least 59 and at most 76.
+    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    pid (Passport ID) - a nine-digit number, including leading zeroes.
+    cid (Country ID) - ignored, missing or not.
+    */
+    // byr, iyr, eyr
+    let re_four_digits = Regex::new(r"(\d{4})").unwrap();
+    let re_height_cm = Regex::new(r"(\d+)cm").unwrap();
+    let re_height_in = Regex::new(r"(\d+)in").unwrap();
+    let re_hair_color = Regex::new(r"#([\da-f]{6})").unwrap();
+    // we're gonna use matching for the eye color.
+    let re_passport_id = Regex::new(r"(\b\d{9}\b)").unwrap();
+
+    let mut num_valid = 0;
+    for passport in parsed {
+        //println!("\nValidating passport: {:?}", passport);
+        if !regex_match(passport.birth_year, &re_four_digits, 1920, 2002) {
+            //println!("Failing birth_year {:?}", passport.birth_year);
+            continue;
+        }
+        if !regex_match(passport.issue_year, &re_four_digits, 2010, 2020) {
+            //println!("Failing issue_year {:?}", passport.issue_year);
+            continue;
+        }
+        if !regex_match(passport.expiration_year, &re_four_digits, 2020, 2030) {
+            //println!("Failing expiration_year {:?}", passport.expiration_year);
+            continue;
+        }
+        if !(regex_match(passport.height, &re_height_cm, 150, 193)
+            || regex_match(passport.height, &re_height_in, 59, 76))
+        {
+            //println!("Failing height {:?}", passport.height);
+            continue;
+        }
+        if regex_is_match(passport.hair_color, &re_hair_color).is_none() {
+            //println!("Failing hair_color {:?}", passport.hair_color);
+            continue;
+        }
+        match passport.eye_color {
+            Some("amb") => {}
+            Some("blu") => {}
+            Some("brn") => {}
+            Some("gry") => {}
+            Some("grn") => {}
+            Some("hzl") => {}
+            Some("oth") => {}
+            _ => {
+                //println!("Failing eye_color {:?}", passport.eye_color);
+                continue;
+            }
+        }
+        if regex_is_match(passport.passport_id, &re_passport_id).is_none() {
+            //println!("Failing passport_id {:?}", passport.passport_id);
+            continue;
+        }
+        println!("Passport OK: {:?}", passport);
+
+        num_valid += 1;
+    }
+
+    num_valid
+}
 
 fn main() {
     println!("=== Advent of Code Day 4 ===");
-    part_one();
-    part_two();
+    println!("= Part One =");
+    validate(&part_one);
+    println!("= Part Two =");
+    validate(&part_two);
 }
