7f66e796cde05f767959cba7404985aab0dd07fb
Marco Jonkers <contact@marcojonkers.com>
2020-12-08 21:51:03 +0100

    Day 8 part two solution

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -106,8 +106,95 @@ fn part_one() -> std::io::Result<i32> {
     Ok(accumulator)
 }
 
+/*
+--- Part Two ---
+
+After some careful analysis, you believe that exactly one instruction is corrupted.
+
+Somewhere in the program, either a jmp is supposed to be a nop, or a nop is supposed to be a jmp.
+(No acc instructions were harmed in the corruption of this boot code.)
+
+The program is supposed to terminate by attempting to execute an instruction immediately after the
+last instruction in the file. By changing exactly one jmp or nop, you can repair the boot code and
+make it terminate correctly.
+
+For example, consider the same program from above:
+
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+
+If you change the first instruction from nop +0 to jmp +0, it would create a single-instruction
+infinite loop, never leaving that instruction. If you change almost any of the jmp instructions, the
+program will still eventually find another jmp instruction and loop forever.
+
+However, if you change the second-to-last instruction (from jmp -4 to nop -4), the program
+terminates! The instructions are visited in this order:
+
+nop +0  | 1
+acc +1  | 2
+jmp +4  | 3
+acc +3  |
+jmp -3  |
+acc -99 |
+acc +1  | 4
+nop -4  | 5
+acc +6  | 6
+
+After the last instruction (acc +6), the program terminates by attempting to run the instruction
+below the last instruction in the file. With this change, after the program terminates, the
+accumulator contains the value 8 (acc +1, acc +1, acc +6).
+
+Fix the program so that it terminates normally by changing exactly one jmp (to nop) or nop (to jmp).
+What is the value of the accumulator after the program terminates?
+*/
+
+fn part_two() -> std::io::Result<i32> {
+    let file = File::open("input")?;
+
+    // Collect all lines into a vector
+    let mut lines: Vec<_> = BufReader::new(file).lines().map(Result::unwrap).collect();
+
+    for i in 0..=lines.len() {
+        let original = lines[i].clone();
+
+        if let Some(line) = lines[i].split(' ').next() {
+            match line {
+                "jmp" => lines[i] = lines[i].replace("jmp", "nop"),
+                "nop" => lines[i] = lines[i].replace("nop", "jmp"),
+                _ => (),
+            }
+        }
+
+        let mut instruction_index = 0;
+        let mut accumulator = 0;
+        let mut history = vec![];
+        while !history.contains(&instruction_index) {
+            history.push(instruction_index);
+            let instruction = &lines[instruction_index as usize];
+            //println!("{}: {}", instruction_index + 1, instruction);
+            instruction_index += execute_instruction(instruction, &mut accumulator);
+
+            if instruction_index as usize == lines.len() {
+                println!("Fixed line {}", i);
+                return Ok(accumulator);
+            }
+        }
+
+        lines[i] = original;
+    }
+
+    Ok(0)
+}
+
 fn main() {
     println!("=== Advent of Code Day 8 ===");
     println!("Part One: {}", part_one().unwrap_or(0));
-    //println!("Part Two: {}", part_two().unwrap_or(0));
+    println!("Part Two: {}", part_two().unwrap_or(0));
 }
