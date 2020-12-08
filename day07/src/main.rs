e02812823718de865f318ee8c73fbf09497908af
Marco Jonkers <contact@marcojonkers.com>
2020-12-08 02:17:29 +0100

    Day 7 part two solution

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -61,7 +61,6 @@ fn expand(map: &HashMap<String, HashSet<String>>, set: &HashSet<String>) -> Hash
     result
 }
 
-// State machine to parse.
 fn part_one() -> std::io::Result<usize> {
     let file = File::open("input")?;
 
@@ -100,9 +99,88 @@ fn part_one() -> std::io::Result<usize> {
     Ok(result)
 }
 
+/*
+--- Part Two ---
+
+It's getting pretty expensive to fly these days - not because of ticket prices, but because of the
+ridiculous number of bags you need to buy!
+
+Consider again your shiny gold bag and the rules from the above example:
+
+    faded blue bags contain 0 other bags.
+    dotted black bags contain 0 other bags.
+    vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+    dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
+
+So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within it) plus 2 vibrant
+plum bags (and the 11 bags within each of those): 1 + 1*7 + 2 + 2*11 = 32 bags!
+
+Of course, the actual rules have a small chance of going several levels deeper than this example; be
+sure to count all of the bags, even if the nesting becomes topologically impractical!
+
+Here's another example:
+
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+
+In this example, a single shiny gold bag must contain 126 other bags.
+
+How many individual bags are required inside your single shiny gold bag?
+*/
+fn count_bags(
+    map: &HashMap<String, HashSet<(String, usize)>>,
+    set: &HashSet<(String, usize)>,
+) -> usize {
+    let mut result = 0;
+    for tuple in set {
+        result += tuple.1;
+        if let Some(list) = map.get(&tuple.0) {
+            result += tuple.1 * count_bags(map, list);
+        }
+    }
+
+    result
+}
+
 fn part_two() -> std::io::Result<usize> {
     let file = File::open("input")?;
+
+    let mut reverse_search = HashMap::new();
+
+    let g = BufReader::new(file)
+        .lines()
+        .filter(|p| !p.as_ref().unwrap().ends_with("no other bags."));
+    for line in g {
+        if let Ok(bag) = line {
+            let mut iter = bag
+                .split(' ')
+                .map(|p| p.replace(&[',', '.'][..], ""))
+                .filter(|p| p != "bags" && p != "bag" && p != "contain")
+                .peekable();
+
+            let container = iter.next().unwrap() + " " + &iter.next().unwrap();
+            //println!("{:?}", container);
+            while iter.peek().is_some() {
+                let count = iter.next().unwrap();
+                let contents = iter.next().unwrap() + " " + &iter.next().unwrap();
+
+                let reverse_set = reverse_search
+                    .entry(container.clone())
+                    .or_insert(HashSet::new());
+                reverse_set.insert((contents.clone(), count.parse::<usize>().unwrap()));
+            }
+        }
+    }
+
     let mut result = 0;
+    if let Some(a) = reverse_search.get("shiny gold") {
+        result = count_bags(&reverse_search, a);
+    }
 
     Ok(result)
 }
