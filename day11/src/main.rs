8d370bb4541b88426371ab7e9073c3d6982ab8a7
Marco Jonkers <contact@marcojonkers.com>
2020-12-12 14:43:44 +0100

    Day 11 part two solution

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -252,7 +252,6 @@ fn part_one() -> std::io::Result<usize> {
                         Position::Floor => '.',
                         Position::EmptySeat => 'L',
                         Position::OccupiedSeat => '#',
-                        _ => unreachable!(),
                     }
                 );
             }
@@ -282,8 +281,310 @@ fn part_one() -> std::io::Result<usize> {
     Ok(num_prev_seats)
 }
 
+/*
+--- Part Two ---
+
+As soon as people start to arrive, you realize your mistake. People don't just care about adjacent
+seats - they care about the first seat they can see in each of those eight directions!
+
+Now, instead of considering just the eight immediately adjacent seats, consider the first seat in
+each of those eight directions. For example, the empty seat below would see eight occupied seats:
+
+.......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#.....
+
+The leftmost empty seat below would only see one empty seat, but cannot see any of the occupied
+ones:
+
+.............
+.L.L.#.#.#.#.
+.............
+
+The empty seat below would see no occupied seats:
+
+.##.##.
+#.#.#.#
+##...##
+...L...
+##...##
+#.#.#.#
+.##.##.
+
+Also, people seem to be more tolerant than you expected: it now takes five or more visible occupied
+seats for an occupied seat to become empty (rather than four or more from the previous rules). The
+other rules still apply: empty seats that see no occupied seats become occupied, seats matching no
+rule don't change, and floor never changes.
+
+Given the same starting layout as above, these new rules cause the seating area to shift around as
+follows:
+
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL
+
+#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##
+
+#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#
+
+#.L#.##.L#
+#L#####.LL
+L.#.#..#..
+##L#.##.##
+#.##.#L.##
+#.#####.#L
+..#.#.....
+LLL####LL#
+#.L#####.L
+#.L####.L#
+
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##LL.LL.L#
+L.LL.LL.L#
+#.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLL#.L
+#.L#LL#.L#
+
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.#L.L#
+#.L####.LL
+..#.#.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#
+
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.LL.L#
+#.LLLL#.LL
+..#.L.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#
+
+Again, at this point, people stop shifting around and the seating area reaches equilibrium. Once
+this occurs, you count 26 occupied seats.
+
+Given the new visibility method and the rule change for occupied seats becoming empty, once
+equilibrium is reached, how many seats end up occupied?
+*/
+
+fn part_two() -> std::io::Result<usize> {
+    let list = BufReader::new(File::open("input")?)
+        .lines()
+        .map(Result::unwrap)
+        .collect::<Vec<_>>();
+
+    let width = list.first().unwrap().chars().count();
+    let height = list.len();
+    println!("width: {}, height: {}", width, height);
+
+    let mut seating_area_chunks = vec![Position::Floor; width * height];
+    let mut seating_area_vec = seating_area_chunks
+        .as_mut_slice()
+        .chunks_mut(width)
+        .collect::<Vec<_>>();
+    let seating_area = seating_area_vec.as_mut_slice();
+
+    for (i, line) in list.iter().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            seating_area[i][j] = match c {
+                '.' => Position::Floor,
+                'L' => Position::EmptySeat,
+                '#' => Position::OccupiedSeat,
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    let mut num_prev_seats = 0;
+    let mut num_iterations = 0;
+    loop {
+        let mut num_seats = 0;
+        //evaluate
+        let mut iteration_chunks = vec![Position::Floor; width * height];
+        let mut iteration_vec = iteration_chunks
+            .as_mut_slice()
+            .chunks_mut(width)
+            .collect::<Vec<_>>();
+        let iteration = iteration_vec.as_mut_slice();
+        // Iterate cells
+        for y in 0..height {
+            for x in 0..width {
+                // Closure to check adjacent cells
+                let adjacent_empty = || {
+                    // 3x3
+                    for i in -1..=1 {
+                        for j in -1..=1 {
+                            if !(i == 0 && j == 0) {
+                                let mut p = y as i32;
+                                let mut q = x as i32;
+
+                                loop {
+                                    p += i;
+                                    q += j;
+                                    if p >= 0 && p < height as i32 && q >= 0 && q < width as i32 {
+                                        // Check for occupied seat
+                                        match seating_area[p as usize][q as usize] {
+                                            Position::OccupiedSeat => return false,
+                                            Position::EmptySeat => break,
+                                            _ => continue,
+                                        }
+                                    } else {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    return true;
+                };
+                let adjacent_occupied = || {
+                    // 3x3
+                    let mut num_occupied = 0;
+                    for i in -1..=1 {
+                        for j in -1..=1 {
+                            if !(i == 0 && j == 0) {
+                                let mut p = y as i32;
+                                let mut q = x as i32;
+
+                                loop {
+                                    p += i;
+                                    q += j;
+                                    if p >= 0 && p < height as i32 && q >= 0 && q < width as i32 {
+                                        // Check for occupied seat
+                                        match seating_area[p as usize][q as usize] {
+                                            Position::OccupiedSeat => {
+                                                num_occupied += 1;
+                                                break;
+                                            }
+                                            Position::EmptySeat => break,
+                                            _ => continue,
+                                        }
+                                    } else {
+                                        break;
+                                    }
+                                }
+                                if num_occupied == 5 {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                    return false;
+                };
+
+                // rules
+                let pos = seating_area[y][x];
+                iteration[y][x] = match pos {
+                    Position::EmptySeat => {
+                        if adjacent_empty() {
+                            Position::OccupiedSeat
+                        } else {
+                            pos
+                        }
+                    }
+                    Position::OccupiedSeat => {
+                        if adjacent_occupied() {
+                            Position::EmptySeat
+                        } else {
+                            pos
+                        }
+                    }
+                    _ => pos,
+                };
+                if iteration[y][x] == Position::OccupiedSeat {
+                    num_seats += 1;
+                }
+            }
+        }
+
+        /*
+        for y in 0..height {
+            for x in 0..width {
+                print!(
+                    "{}",
+                    match iteration[y][x] {
+                        Position::Floor => '.',
+                        Position::EmptySeat => 'L',
+                        Position::OccupiedSeat => '#',
+                    }
+                );
+            }
+            println!();
+        }
+        println!();
+        */
+
+        num_iterations += 1;
+        /*
+        println!(
+            "num_iterations: {}, num_seats: {}, num_prev_seats: {}",
+            num_iterations, num_seats, num_prev_seats
+        );
+
+        // DEBUG
+        if num_iterations == 7 {
+            break;
+        }
+        */
+        if num_seats == num_prev_seats {
+            break;
+        }
+        num_prev_seats = num_seats;
+        for y in 0..height {
+            for x in 0..width {
+                seating_area[y][x] = iteration[y][x];
+            }
+        }
+    }
+    println!("Iterations: {}", num_iterations);
+
+    Ok(num_prev_seats)
+}
+
 fn main() {
     println!("=== Advent of Code Day 11 ===");
     println!("Part One: {}", part_one().unwrap_or(0));
-    //println!("Part Two: {}", part_two().unwrap_or(0));
+    println!("Part Two: {}", part_two().unwrap_or(0));
 }
